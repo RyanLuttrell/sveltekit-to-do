bc3101f506c4d4d9f0655e2eec2fe51f8cb97041
Ryan Luttrell <ryan.m.luttrell@gmail.com>
2022-01-31 15:13:39 -0400

    initial commit and svelte/tailwind setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  theme: {
+    colors: {
+      white: colors.white,
+      gray: colors.stone,
+      'svelte-prime': '#ff5030',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
